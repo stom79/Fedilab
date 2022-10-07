10f2ac396c2b875ccb903c3aff5583bb393f071a
Thomas <tschneider.ac@gmail.com>
2022-10-07 18:49:54 +0200

    Fix issue #338 #315 - Copy/Paste in threads

--- app/src/main/java/app/fedilab/android/activities/ComposeActivity.java
+++ app/src/main/java/app/fedilab/android/activities/ComposeActivity.java
@@ -245,7 +245,7 @@ public class ComposeActivity extends BaseActivity implements ComposeAdapter.Mana
         statusList.addAll(0, context.ancestors);
         composeAdapter.setStatusCount(context.ancestors.size() + 1);
         composeAdapter.notifyItemRangeInserted(0, context.ancestors.size());
-        composeAdapter.notifyItemChanged(context.ancestors.size() + 1);
+     //   composeAdapter.notifyItemChanged(context.ancestors.size() + 1);
         if (binding.recyclerView.getItemDecorationCount() > 0) {
             for (int i = 0; i < binding.recyclerView.getItemDecorationCount(); i++) {
                 binding.recyclerView.removeItemDecorationAt(i);
