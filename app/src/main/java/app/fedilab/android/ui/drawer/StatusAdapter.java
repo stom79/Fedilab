7dfa1f681b515849a3a7818ba0d006a04a39a224
Thomas <tschneider.ac@gmail.com>
2022-11-02 19:13:05 +0100

    ui issue with edit date

--- app/src/main/java/app/fedilab/android/ui/drawer/StatusAdapter.java
+++ app/src/main/java/app/fedilab/android/ui/drawer/StatusAdapter.java
@@ -1002,6 +1002,7 @@ public class StatusAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             holder.binding.dateShort.setVisibility(View.GONE);
             holder.binding.visibilitySmall.setVisibility(View.GONE);
         } else {
+            holder.binding.editTime.setVisibility(View.GONE);
             holder.binding.visibilitySmall.setImageResource(ressource);
             if (displayCounters && canBeFederated) {
                 holder.binding.statusInfo.setVisibility(View.VISIBLE);
