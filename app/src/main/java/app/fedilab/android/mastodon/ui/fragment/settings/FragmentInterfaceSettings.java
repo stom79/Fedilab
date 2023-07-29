a43a97d960c93e5a2489c051e24ea4be1788b055
Thomas <tschneider.ac@gmail.com>
2023-07-29 12:12:35 +0200

    Add new logo

--- app/src/main/java/app/fedilab/android/mastodon/ui/fragment/settings/FragmentInterfaceSettings.java
+++ app/src/main/java/app/fedilab/android/mastodon/ui/fragment/settings/FragmentInterfaceSettings.java
@@ -150,6 +150,14 @@ public class FragmentInterfaceSettings extends PreferenceFragmentCompat implemen
         context.getPackageManager().setComponentEnabledSetting(
                 new ComponentName(BuildConfig.APPLICATION_ID, "app.fedilab.android.activities.MainActivity.Mastalab"),
                 PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+
+        context.getPackageManager().setComponentEnabledSetting(
+                new ComponentName(BuildConfig.APPLICATION_ID, "app.fedilab.android.activities.MainActivity.BubblesUA"),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+
+        context.getPackageManager().setComponentEnabledSetting(
+                new ComponentName(BuildConfig.APPLICATION_ID, "app.fedilab.android.activities.MainActivity.BubblesPeaGreen"),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
     }
 
     private void setIcon(Context context, String iconName) {
