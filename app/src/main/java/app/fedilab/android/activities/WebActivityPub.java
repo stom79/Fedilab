6b37ac2eb5c1fc62ae1c15ba2adab6c80fd731c2
Thomas <tschneider.ac@gmail.com>
2026-05-05 14:45:20 +0200

    - Fix web+ap URL account switching

--- app/src/main/java/app/fedilab/android/activities/WebActivityPub.java
+++ app/src/main/java/app/fedilab/android/activities/WebActivityPub.java
@@ -15,22 +15,35 @@ package app.fedilab.android.activities;
  * see <http://www.gnu.org/licenses>. */
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.preference.PreferenceManager;
 
-import app.fedilab.android.mastodon.activities.ContextActivity;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.fedilab.android.BaseMainActivity;
+import app.fedilab.android.activities.MainActivity;
+import app.fedilab.android.mastodon.activities.BaseActivity;
 import app.fedilab.android.mastodon.activities.ProfileActivity;
 import app.fedilab.android.mastodon.client.entities.api.Account;
-import app.fedilab.android.mastodon.client.entities.api.Status;
+import app.fedilab.android.mastodon.client.entities.app.BaseAccount;
 import app.fedilab.android.mastodon.client.entities.app.CachedBundle;
-import app.fedilab.android.mastodon.helper.CrossActionHelper;
+import app.fedilab.android.mastodon.exception.DBException;
 import app.fedilab.android.mastodon.helper.Helper;
+import app.fedilab.android.mastodon.ui.drawer.AccountsSearchAdapter;
+import app.fedilab.android.R;
 
 
-public class WebActivityPub extends AppCompatActivity {
+public class WebActivityPub extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -69,11 +82,15 @@ public class WebActivityPub extends AppCompatActivity {
         boolean isStatus = false;
 
         if (path.startsWith("/@")) {
-            String[] params = path.split("@");
-            if (params.length == 2) {
-                acct = params[1] + "@" + host;
-            } else if (params.length >= 2 && path.matches("/@[^/]+/\\d+")) {
+            if (path.matches("/@[^/]+/\\d+")) {
                 isStatus = true;
+            } else {
+                String[] params = path.split("@");
+                if (params.length == 2) {
+                    acct = params[1] + "@" + host;
+                } else if (params.length == 3) {
+                    acct = params[1] + "@" + params[2];
+                }
             }
         } else if (path.split("/").length > 2) {
             String[] params = path.split("/");
@@ -86,23 +103,84 @@ public class WebActivityPub extends AppCompatActivity {
         }
 
         if (acct != null) {
-            openProfile(acct);
+            String finalAcct = acct;
+            chooseAccount(account -> openProfile(account, finalAcct));
         } else if (isStatus) {
-            openStatus(httpsUrl);
+            String finalHttpsUrl = httpsUrl;
+            chooseAccount(account -> openStatus(account, finalHttpsUrl));
         } else {
             finish();
         }
     }
 
-    private void openProfile(String acct) {
-        if (Helper.getCurrentAccount(WebActivityPub.this) == null) {
-            finish();
-            return;
-        }
+    private interface AccountSelectedCallback {
+        void onAccountSelected(BaseAccount account);
+    }
+
+    private void chooseAccount(AccountSelectedCallback callback) {
+        new Thread(() -> {
+            try {
+                List<BaseAccount> accounts = new app.fedilab.android.mastodon.client.entities.app.Account(WebActivityPub.this).getCrossAccounts();
+                if (accounts == null || accounts.isEmpty()) {
+                    runOnUiThread(this::finish);
+                    return;
+                }
+                if (accounts.size() == 1) {
+                    runOnUiThread(() -> callback.onAccountSelected(accounts.get(0)));
+                } else {
+                    List<Account> accountList = new ArrayList<>();
+                    for (BaseAccount account : accounts) {
+                        account.mastodon_account.acct += "@" + account.instance;
+                        accountList.add(account.mastodon_account);
+                    }
+                    Handler mainHandler = new Handler(Looper.getMainLooper());
+                    mainHandler.post(() -> {
+                        AlertDialog.Builder builderSingle = new MaterialAlertDialogBuilder(WebActivityPub.this);
+                        builderSingle.setTitle(getString(R.string.choose_accounts));
+                        final AccountsSearchAdapter accountsSearchAdapter = new AccountsSearchAdapter(WebActivityPub.this, accountList);
+                        final BaseAccount[] accountArray = new BaseAccount[accounts.size()];
+                        int i = 0;
+                        for (BaseAccount account : accounts) {
+                            accountArray[i] = account;
+                            i++;
+                        }
+                        builderSingle.setNegativeButton(R.string.cancel, (dialog, which) -> {
+                            dialog.dismiss();
+                            finish();
+                        });
+                        builderSingle.setOnCancelListener(dialog -> finish());
+                        builderSingle.setAdapter(accountsSearchAdapter, (dialog, which) -> {
+                            callback.onAccountSelected(accountArray[which]);
+                            dialog.dismiss();
+                        });
+                        builderSingle.show();
+                    });
+                }
+            } catch (DBException e) {
+                e.printStackTrace();
+                runOnUiThread(this::finish);
+            }
+        }).start();
+    }
+
+    private void switchAccount(BaseAccount selectedAccount) {
+        BaseMainActivity.currentToken = selectedAccount.token;
+        BaseMainActivity.currentUserID = selectedAccount.user_id;
+        BaseMainActivity.currentInstance = selectedAccount.instance;
+        Helper.setCurrentAccount(selectedAccount);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putString(Helper.PREF_USER_TOKEN, selectedAccount.token);
+        editor.putString(Helper.PREF_USER_INSTANCE, selectedAccount.instance);
+        editor.putString(Helper.PREF_USER_ID, selectedAccount.user_id);
+        editor.commit();
+    }
+
+    private void openProfile(BaseAccount selectedAccount, String acct) {
+        switchAccount(selectedAccount);
         Intent intentProfile = new Intent(WebActivityPub.this, ProfileActivity.class);
         Bundle args = new Bundle();
         args.putString(Helper.ARG_MENTION, acct);
-        new CachedBundle(WebActivityPub.this).insertBundle(args, Helper.getCurrentAccount(WebActivityPub.this), bundleId -> {
+        new CachedBundle(WebActivityPub.this).insertBundle(args, selectedAccount, bundleId -> {
             Bundle bundle = new Bundle();
             bundle.putLong(Helper.ARG_INTENT_ID, bundleId);
             intentProfile.putExtras(bundle);
@@ -112,34 +190,13 @@ public class WebActivityPub extends AppCompatActivity {
         });
     }
 
-    private void openStatus(String httpsUrl) {
-        if (Helper.getCurrentAccount(WebActivityPub.this) == null) {
-            finish();
-            return;
-        }
-        CrossActionHelper.fetchRemoteStatus(WebActivityPub.this, Helper.getCurrentAccount(WebActivityPub.this), httpsUrl, new CrossActionHelper.Callback() {
-            @Override
-            public void federatedStatus(Status status) {
-                if (status != null) {
-                    Intent intentContext = new Intent(WebActivityPub.this, ContextActivity.class);
-                    Bundle args = new Bundle();
-                    args.putSerializable(Helper.ARG_STATUS, status);
-                    new CachedBundle(WebActivityPub.this).insertBundle(args, Helper.getCurrentAccount(WebActivityPub.this), bundleId -> {
-                        Bundle bundle = new Bundle();
-                        bundle.putLong(Helper.ARG_INTENT_ID, bundleId);
-                        intentContext.putExtras(bundle);
-                        intentContext.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intentContext);
-                        finish();
-                    });
-                } else {
-                    finish();
-                }
-            }
-
-            @Override
-            public void federatedAccount(Account account) {
-            }
-        });
+    private void openStatus(BaseAccount selectedAccount, String httpsUrl) {
+        switchAccount(selectedAccount);
+        Intent mainActivity = new Intent(WebActivityPub.this, MainActivity.class);
+        mainActivity.putExtra(Helper.INTENT_ACTION, Helper.OPEN_WITH_ANOTHER_ACCOUNT);
+        mainActivity.putExtra(Helper.PREF_MESSAGE_URL, httpsUrl);
+        mainActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainActivity);
+        finish();
     }
 }
